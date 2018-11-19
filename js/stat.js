2f473f06abaf10091c6bc01432d4d9ae72800dbd
Дмитрий <selyxaa@yandex.ru>
2018-11-19 22:33:01 +0300

    исправил замечания

--- js/stat.js
+++ js/stat.js
@@ -9,15 +9,17 @@ var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var HISTOGRAM_HEIGHT = 150;
 var TEXT_HEIGHT = 16;
+var FONT_STYLE = '16px PT Mono';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
+
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
-  for (var i = 0; i < arr.length; i++) {
+  for (var i = 1; i < arr.length; i++) {
     if (arr[i] > maxElement) {
       maxElement = arr[i];
     }
@@ -30,24 +32,43 @@ window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  ctx.font = '16px PT Mono';
+  ctx.font = FONT_STYLE;
   ctx.fillStyle = 'rgba(0, 0, 0, 1)';
   ctx.fillText('Ура вы победили!', 200, 30);
   ctx.fillText('Список результатов:', 200, 45);
   var maxTime = getMaxElement(times);
-  for (var i = 0; i < players.length; i++) {
-    var timesRound = Math.round(times[i]);
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - GAP);
-    ctx.fillText(timesRound, CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - GAP * 2 - TEXT_HEIGHT - (HISTOGRAM_HEIGHT * times[i]) / maxTime);
-    if (players[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      var getRandomInRange = function (min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
+  var renderHistogram = function () {
+    for (var i = 0; i < players.length; i++) {
+      var timesRound = Math.round(times[i]);
+      var coordinateX = CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i;
+      var textY = CLOUD_Y + CLOUD_HEIGHT - GAP;
+      var barHeight = HISTOGRAM_HEIGHT * times[i] / maxTime;
+      var timesY = textY - GAP - TEXT_HEIGHT - barHeight;
+      var barY = textY - TEXT_HEIGHT - barHeight;
+
+      var renderBar = function () {
+        ctx.fillRect(coordinateX, barY, BAR_WIDTH, barHeight);
       };
-      ctx.fillStyle = 'rgba(' + getRandomInRange(0, 150) + ', ' + getRandomInRange(0, 150) + ', 255, 1)';
+
+      var getRandomBlue = function (min, max) {
+        var randomColorValue = Math.floor(Math.random() * (max - min)) + min;
+        var randomBlue = 'rgba(' + randomColorValue + ', ' + randomColorValue + ', 255, 1)';
+        return randomBlue;
+      };
+
+      ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+      ctx.fillText(players[i], coordinateX, textY);
+      ctx.fillText(timesRound, coordinateX, timesY);
+
+      if (players[i] === 'Вы') {
+        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      } else {
+        ctx.fillStyle = getRandomBlue(0, 150);
+      }
+
+      renderBar();
     }
-    ctx.fillRect(CLOUD_X + BAR_GAP + ((BAR_GAP + BAR_WIDTH) * i), CLOUD_Y + CLOUD_HEIGHT - GAP - TEXT_HEIGHT - (HISTOGRAM_HEIGHT * times[i]) / maxTime, BAR_WIDTH, (HISTOGRAM_HEIGHT * times[i]) / maxTime);
-  }
+  };
+
+  renderHistogram();
 };
